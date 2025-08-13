4792e95610d789413582fbff0a9d8c6c767abc77
Dejoyag782 <dejoyag782@gmail.com>
2025-08-14 00:03:34 +0800

    Removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Peer, { MediaConnection, DataConnection } from "peerjs";
 
 export default function App() {
